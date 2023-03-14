150bf4aa0a44581659d52ef3b79866d7edf8db89
Akarshan Jaiswal <akarshanjaiswal@gmail.com>
2023-03-14 17:57:57 +0000

    streamgraph_generic

--- JS/scatterplot.js
+++ JS/scatterplot.js
@@ -4,10 +4,11 @@ const scatter_plot_margin = {top: 10, right: 30, bottom: 30, left: 60},
     scatter_plot_height = window.innerHeight - scatter_plot_margin.top - scatter_plot_margin.bottom;
 
 // append the svg object to the body of the page
-const scatter_plot_svg = d3.select("#my_scatter_plot")
+const scatter_plot_svg = d3.select("#scatterplot_div")
   .append("svg")
     .attr("width", scatter_plot_width + scatter_plot_margin.left + scatter_plot_margin.right)
     .attr("height", scatter_plot_height + scatter_plot_margin.top + scatter_plot_margin.bottom)
+    .attr("id","scatter_plot")
   .append("g")
     .attr("transform",
           `translate(${scatter_plot_margin.left}, ${scatter_plot_margin.top})`);
@@ -18,62 +19,69 @@ const data_path=git_path+"Dataset/Processed_files/Time_distributed/total_cases.c
 //createScatterPlot("Africa","Africa",scatter_plot_width,scatter_plot_height,scatter_plot_svg,data_path,"scatter_input")
 createScatterPlot(selected_country,selected_country,scatter_plot_width,scatter_plot_height,scatter_plot_svg,data_path,"scatter_input")
 function createScatterPlot(parameter1,parameter2,chart_width,chart_height,chart_svg,csv_path,input_id){
-    
-//Read the data
-d3.csv(csv_path).then( function(data) {
+    chart_svg.selectAll("g").remove();
 
-    // Add X axis
-    const scatter_plot_x = d3.scaleLinear()
-    .domain([3, 9])
-    .range([ 0, chart_width ]);
-    const scatter_plot_xAxis = chart_svg.append("g")
-    .attr("transform", `translate(0, ${chart_height})`)
-    .call(d3.axisBottom(scatter_plot_x));
+    chart_svg.append("svg")
+        .attr("width", chart_width + scatter_plot_margin.left + scatter_plot_margin.right)
+        .attr("height", chart_height + scatter_plot_margin.top + scatter_plot_margin.bottom)
+        .attr("id","scatter_plot")
+        .append("g")
+        .attr("transform",
+                `translate(${scatter_plot_margin.left}, ${scatter_plot_margin.top})`);
+        
+    //Read the data
+    d3.csv(csv_path).then( function(data) {
+        // Add X axis
+        const scatter_plot_x = d3.scaleLinear()
+        .domain([3, 9])
+        .range([ 0, chart_width ]);
+        const scatter_plot_xAxis = chart_svg.append("g")
+        .attr("transform", `translate(0, ${chart_height})`)
+        .call(d3.axisBottom(scatter_plot_x));
 
-    // Add Y axis
-    const scatter_plot_y = d3.scaleLinear()
-    .domain([0, 9])
-    .range([ chart_height, 0]);
-    chart_svg.append("g")
-    .call(d3.axisLeft(scatter_plot_y));
+        // Add Y axis
+        const scatter_plot_y = d3.scaleLinear()
+        .domain([0, 9])
+        .range([ chart_height, 0]);
+        chart_svg.append("g")
+        .call(d3.axisLeft(scatter_plot_y));
 
-    // Add dots
-    chart_svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .join("circle")
-        .attr("cx", function (d) { return scatter_plot_x(d[parameter1]); } )
-        .attr("cy", function (d) { return scatter_plot_y(d[parameter2]); } )
-        .attr("r", 10)
-        .style("fill", "#radial-gradient(circle, rgba(32,123,142,1) 0%, rgba(9,33,121,1) 48%, rgba(2,0,36,1) 88%)" )
-        .style("stroke", "#69b3a2" )
-        .style("transform", "scale(0.001, 0.001)")
-        .transition()
-        .duration(1500)
-        .style("transform", null)
-
-
-    // A function that update the plot for a given xlim value
-    function updatePlot() {
-
-    // Get the value of the button
-    xlim = this.value
-
-    // Update X axis
-    scatter_plot_x.domain([3,xlim])
-    scatter_plot_xAxis.transition().duration(1000).call(d3.axisBottom(scatter_plot_x))
-
-    // Update chart
-    chart_svg.selectAll("circle")
+        // Add dots
+        chart_svg.append('g')
+        .selectAll("dot")
         .data(data)
-        .transition()
-        .duration(1000)
-        .attr("cx", function (d) { return scatter_plot_x(d[parameter1]); } )
-        .attr("cy", function (d) { return scatter_plot_y(d[parameter2]); } )
-    }
+        .join("circle")
+            .attr("cx", function (d) { return scatter_plot_x(d[parameter1]); } )
+            .attr("cy", function (d) { return scatter_plot_y(d[parameter2]); } )
+            .attr("r", 10)
+            .style("fill", "#radial-gradient(circle, rgba(32,123,142,1) 0%, rgba(9,33,121,1) 48%, rgba(2,0,36,1) 88%)" )
+            .style("stroke", "#69b3a2" )
+            .style("transform", "scale(0.001, 0.001)")
+            .transition()
+            .duration(1500)
+            .style("transform", null)
 
-    // Add an event listener to the button created in the html part
-    d3.select("#"+input_id).on("input", updatePlot )
 
+        // A function that update the plot for a given xlim value
+        function updatePlot() {
+
+        // Get the value of the button
+        xlim = this.value
+
+        // Update X axis
+        scatter_plot_x.domain([3,xlim])
+        scatter_plot_xAxis.transition().duration(1000).call(d3.axisBottom(scatter_plot_x))
+
+        // Update chart
+        chart_svg.selectAll("circle")
+            .data(data)
+            .transition()
+            .duration(1000)
+            .attr("cx", function (d) { return scatter_plot_x(d[parameter1]); } )
+            .attr("cy", function (d) { return scatter_plot_y(d[parameter2]); } )
+        }
+
+        // Add an event listener to the button created in the html part
+        d3.select("#"+input_id).on("input", updatePlot )
     })
 }
---
+++ JS/streamgraph.js
@@ -0,0 +1,109 @@
+
+// set the dimensions and margins of the graph
+const streamgraph_margin = {top: 20, right: 30, bottom: 0, left: 10},
+    streamgraph_width = window.innerWidth - streamgraph_margin.left - streamgraph_margin.right,
+    streamgraph_height = window.innerHeight - streamgraph_margin.top - streamgraph_margin.bottom;
+
+// append the svg object to the body of the page
+const streamgraph_svg = d3.select("#streamgraph_div")
+  .append("svg")
+    .attr("width", streamgraph_width + streamgraph_margin.left + streamgraph_margin.right)
+    .attr("height", streamgraph_height + streamgraph_margin.top + streamgraph_margin.bottom)
+  .append("g")
+    .attr("transform",
+          `translate(${streamgraph_margin.left}, ${streamgraph_margin.top})`);
+const dummy_path="https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv";
+createStreamGraph(streamgraph_height,streamgraph_width,streamgraph_svg,dummy_path);
+function createStreamGraph(chart_height,chart_width,chart_svg,csv_path){
+    chart_svg.selectAll("g").remove();
+    chart_svg.append("svg")
+        .attr("width", streamgraph_width + streamgraph_margin.left + streamgraph_margin.right)
+        .attr("height", streamgraph_height + streamgraph_margin.top + streamgraph_margin.bottom)
+    .append("g")
+        .attr("transform",
+            `translate(${streamgraph_margin.left}, ${streamgraph_margin.top})`);
+    // Parse the Data
+    d3.csv(csv_path).then(function(data) {
+
+    // List of groups = header of the csv files
+    const keys = data.columns.slice(1)
+
+    // Add X axis
+    const x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.year; }))
+    .range([ 0, chart_width ]);
+    chart_svg.append("g")
+    .attr("transform", `translate(0, ${chart_height*0.8})`)
+    .call(d3.axisBottom(x).tickSize(-chart_height*.7).tickValues([1900, 1925, 1975, 2000]))
+    .select(".domain").remove()
+    // Customization
+    chart_svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
+
+    // Add X axis label:
+    chart_svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", chart_width)
+        .attr("y", chart_height-30 )
+        .text("Time (year)");
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+    .domain([-100000, 100000])
+    .range([ chart_height, 0 ]);
+
+    // color palette
+    const color = d3.scaleOrdinal()
+    .domain(keys)
+    .range(d3.schemeDark2);
+
+    //stack the data?
+    const stackedData = d3.stack()
+    .offset(d3.stackOffsetSilhouette)
+    .keys(keys)
+    (data)
+
+    // create a tooltip
+    const Tooltip = chart_svg
+    .append("text")
+    .attr("x", 0)
+    .attr("y", 0)
+    .style("opacity", 0)
+    .style("font-size", 17)
+
+    // Three function that change the tooltip when user hover / move / leave a cell
+    const mouseover = function(event,d) {
+    Tooltip.style("opacity", 1)
+    d3.selectAll(".myArea").style("opacity", .2)
+    d3.select(this)
+        .style("stroke", "black")
+        .style("opacity", 1)
+    }
+    const mousemove = function(event,d,i) {
+    grp = d.key
+    Tooltip.text(grp)
+    }
+    const mouseleave = function(event,d) {
+    Tooltip.style("opacity", 0)
+    d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
+    }
+
+    // Area generator
+    const area = d3.area()
+    .x(function(d) { return x(d.data.year); })
+    .y0(function(d) { return y(d[0]); })
+    .y1(function(d) { return y(d[1]); })
+
+    // Show the areas
+    chart_svg
+    .selectAll("mylayers")
+    .data(stackedData)
+    .join("path")
+        .attr("class", "myArea")
+        .style("fill", function(d) { return color(d.key); })
+        .attr("d", area)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+
+    })
+}
