1472b715f4487d1c2f44509deeb61a9494a64e7b
Akarshan Jaiswal <akarshanjaiswal@gmail.com>
2023-04-07 21:23:16 +0100

    updated

--- JS/choropleth.js
+++ JS/choropleth.js
@@ -1,6 +1,5 @@
 //Making Map
 const choropleth_svg = d3.select("#choropleth_div").append("svg")
-//.style('background','radial-gradient(circle, rgba(32,123,142,1) 0%, rgba(9,33,121,1) 48%, rgba(2,0,36,1) 88%)');
 .style('background','radial-gradient(circle, rgba(2,0,36,1) 8%, rgba(41,78,209,1) 31%, rgba(3,7,8,1) 82%)');
     choropleth_svg.attr("width",window.innerWidth);
     choropleth_svg.attr("height",window.innerHeight-100);
@@ -12,22 +11,25 @@ const choropleth_svg = d3.select("#choropleth_div").append("svg")
     [1000,10000,50000,100000,250000,750000,1000000,50000000],git_path+"Resources/GeoJsons/Extensive.geojson",
     git_path+"Dataset/Average_cases/total.csv",0,["",""],0)
 */
+var selected_country = ""
 choropleth_plotter(choropleth_width,choropleth_height,choropleth_svg,
   [1000,10000,50000,100000,250000,750000,1000000,50000000],
-  "https://raw.githubusercontent.com/Akarshan-Jaiswal/datavisualizationandanalytics.github.io/CW1/Resources/GeoJsons/Extensive.geojson",
+  "https://raw.githubusercontent.com/Akarshan-Jaiswal/datavisualizationandanalytics.github.io/CW1/Resources/GeoJsons/world.geojson",
   "https://raw.githubusercontent.com/Akarshan-Jaiswal/datavisualizationandanalytics.github.io/22d80dbd66f2a54936e53e4f019b3f188504c760/Dataset/Average_cases/total.csv",
-  ["location","total_cases"],1,[["",""]])
+  ["location","total_cases"],0,[["",""]])
   function choropleth_plotter(map_width,map_height,map_svg,scale_domain,geojson_path,csv_path,csv_var,map_state,map_funtions){
   // Map and projection
-  const map_path = d3.geoPath();
+  //const map_path = d3.geoPath();
+  var stop_rotation = false;
+  var is_clicked =false;
+  const initialScale = 250;
   const map_projection = d3.geoOrthographic()
-    .scale(300)
+    .scale(initialScale)
     .center([0,0])
     .rotate([0,-30])
     .translate([map_width / 2, map_height / 2]);
-  const initialScale = map_projection.scale()
-    //let path = d3.geoPath().projection(map_projection)
-  let globe = choropleth_svg.append("circle")
+
+  let globe = map_svg.append("circle")
     .attr("fill", "#1e1e5f")
     //.attr("background-image", "radial-gradient(circle, rgba(32,123,142,1) 0%, rgba(9,33,121,1) 48%, rgba(2,0,36,1) 88%)")
     .attr("stroke", "#000")
@@ -35,6 +37,37 @@ choropleth_plotter(choropleth_width,choropleth_height,choropleth_svg,
     .attr("cx", map_width/2)
     .attr("cy", map_height/2)
     .attr("r", initialScale)
+
+  // globe.call(d3.drag().on('drag', () => {
+  //   const rotate = map_projection.rotate()
+  //   const sensitivity = 90
+  //   const k = sensitivity / map_projection.scale()
+  //   map_projection.rotate([
+  //     rotate[0] + d3.pointer(event)[0] * k,
+  //     rotate[1] - d3.pointer(event)[1] * k
+  //   ])
+  //   path = d3.geoPath().projection(map_projection)
+  //   map_svg.selectAll("path").attr("d", path)
+  // }))
+  globe.call(d3.drag().on('drag', (event) => {
+    const rotate = map_projection.rotate();
+    const lambda = d3.scaleLinear()
+        .domain([-map_width / 2, map_width / 2])
+        .range([-180, 180]);
+    const phi = d3.scaleLinear()
+        .domain([-map_height / 2, map_height / 2])
+        .range([90, -90]);
+    const sensitivity = 100;
+    const dx = event.dx * sensitivity / map_width;
+    const dy = event.dy * sensitivity / map_height;
+    const new_rotation = [
+      lambda(dx) + rotate[0],
+      phi(dy) + rotate[1]
+    ];
+    map_projection.rotate(new_rotation);
+    path = d3.geoPath().projection(map_projection);
+    map_svg.selectAll("path").attr("d", path);
+}));
     
   
   // Data and color scale
@@ -52,44 +85,59 @@ choropleth_plotter(choropleth_width,choropleth_height,choropleth_svg,
   
       //Hover functions
       let mouseOver = function(d) {
-      d3.selectAll(".Country")
+      if(!is_clicked){
+        d3.selectAll(".Country")
         .transition()
         .duration(200)
         .style("opacity", .3)
-      d3.select(this)
+        d3.select(this)
         .transition()
         .duration(200)
         .style("opacity", 1)
         .style("stroke", "black")
         console.log(d3.select(this).attr("id"),d3.select(this).attr("fill"))
+        //map_projection.scale(2.7*((initialScale*2)/5));
+      }
     }
   
     let mouseLeave = function(d) {
-      d3.selectAll(".Country")
-        .transition()
-        .duration(200)
-        .style("opacity", 1)
-      d3.select(this)
-        .transition()
-        .duration(200)
-        .style("stroke", "")
+      if(!is_clicked){
+          d3.selectAll(".Country")
+          .transition()
+          .duration(200)
+          .style("opacity", 1)
+          d3.select(this)
+          .transition()
+          .duration(200)
+          .style("stroke", "")
+          //map_projection.scale(initialScale);
+          globe.attr("r",initialScale);
+      }
     }
   
     //Click function
     let countryClick = function(d) {
-      d3.selectAll(".Country")
-        .transition()
-        .duration(200)
-        .style("opacity", 0);
-      d3.select(this)
-        .transition()
-        .duration(200)
-        .style("opacity", 1)
-        .style("stroke", "black")
+      if (!is_clicked){
+        d3.selectAll(".Country")
+          .transition()
+          .duration(200)
+          .style("opacity", 0);
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .style("opacity", 1)
+          .style("stroke", "black")
       selected_country=d3.select(this).attr("id");
       d3.select('body').transition()
-      .duration(1500)
-      .style("transform", null);
+        .duration(1500)
+        .style("transform", null);
+      stop_rotation = true;
+      is_clicked = true;
+      globe.attr("r",0);
+      }else{
+        stop_rotation = false;
+        is_clicked = false;
+      }
     }
   
     // Draw the map
@@ -123,5 +171,26 @@ choropleth_plotter(choropleth_width,choropleth_height,choropleth_svg,
             final_map.on(element[0],element[1])
           });
         }
+
+    //Optional rotate
+  let then = Date.now();
+  d3.timer(function(elapsed) {
+      const now = Date.now();
+      const delta = now - then;
+      then = now;
+      if (!stop_rotation){
+        const rotate = map_projection.rotate();
+        const k = 75 / map_projection.scale();
+        map_projection.rotate([
+            rotate[0] - delta * 0.01 * k,
+            rotate[1]
+        ]);
+        path = d3.geoPath().projection(map_projection);
+        map_svg.selectAll("path").attr("d", path);
+        //map_projection.scale(initialScale);
+        }else{
+          map_projection.scale(2.7*((initialScale*2)/5));
+        }
+  }, 200);
   })
   }
